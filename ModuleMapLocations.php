2ca9c76be390646df064b686c5b2657f1fe191c1
Andreas Schempp <andreas@schempp.ch>
2012-05-22 18:00:12 +0200

    Updated module with new Contao copyright

--- ModuleMapLocations.php
+++ ModuleMapLocations.php
@@ -1,13 +1,15 @@
 <?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
 
 /**
- * TYPOlight webCMS
- * Copyright (C) 2005-2009 Leo Feyer
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
- * version 2.1 of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,21 +18,26 @@
  * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
- * Software Foundation website at http://www.gnu.org/licenses/.
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
  *
  * PHP version 5
- * @copyright  Andreas Schempp 2009
+ * @copyright  Andreas Schempp 2008-2010
  * @author     Andreas Schempp <andreas@schempp.ch>
  * @license    http://opensource.org/licenses/lgpl-3.0.html
- * @version    $Id$
+ * @version    $Id: $
  */
 
  
 class ModuleMapLocations extends Module
 {
 
+	/**
+	 * Template
+	 * @var string
+	 */
 	protected $strTemplate = 'map_default';
 
+	
 	public function generate()
 	{
 		if (TL_MODE == 'BE')
@@ -120,7 +127,6 @@ class ModuleMapLocations extends Module
 			$this->Template->locations = array();
 		
 		$this->Template->pinLabel = $GLOBALS['TL_LANG']['MSC']['label_pin'];
-		$this->Template->id = $this->id;
 		
 		$GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/maplocations/html/maplocations.js';
 	}
